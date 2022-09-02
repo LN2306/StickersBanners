3bc962b2013394738d643982fbe4e895c8bedc12
LN2306 <112431162+LN2306@users.noreply.github.com>
2022-09-02 11:18:15 -0500

    Add function to read the sales rep file and initiate the sales Class for each of them

---
+++ readJob.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 10:13:13 2022
+
+@author: tuong
+"""
+from salesClass import Sales
+
+def read_file(directory):
+    job = open(directory , "r")
+    content = job.readlines()
+    job.close()
+    sales_list = []
+    for line in content:
+        name, ID = line.split(" ")
+        temp = Sales(name, ID ,4,4,4,4,4)
+        exec(name + " = temp")
+        exec("sales_list.append(" + name + ")")
+    return sales_list
+
+sales_list = read_file("sales.txt")
+
+    
