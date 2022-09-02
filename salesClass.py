740a40a9eea863497d72ea531a4f83e64057aff4
LN2306 <112431162+LN2306@users.noreply.github.com>
2022-09-02 09:32:20 -0500

    Add Class for the Sales Rep

---
+++ salesClass.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep  2 08:21:19 2022
+
+@author: tuong
+"""
+
+class Sales:
+    def __init__(self, name, ID, broker, no_file, design, reg, no_proof):
+        self._name = name
+        self._ID = ID
+        self._broker = broker
+        self._no_file = no_file
+        self._design = design
+        self._reg = reg
+        self._no_proof = no_proof
+    
+    @property
+    def broker(self):
+        return self._broker
+    @broker.setter
+    def broker(self, freq):
+        self._broker = freq
+
+    @property
+    def no_file(self):
+        return self._no_file
+    @no_file.setter
+    def no_file(self, freq):
+        self._no_file = freq
+        
+    @property
+    def design(self):
+        return self._design
+    @design.setter
+    def design(self, freq):
+        self._design = freq
+        
+    @property
+    def reg(self):
+        return self._req
+    @reg.setter
+    def reg(self, freq):
+        self._reg = freq
+        
+    @property
+    def no_proof(self):
+        return self._no_proof
+    @no_proof.setter
+    def no_proof(self, freq):
+        self._no_proof = freq
